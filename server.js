0b00d6a85629e622cc1d996b0eb4a4fafc07a489
Amna Shoukat <amnashoukat80@gmail.com>
2023-01-21 00:42:09 +0500

    "commit 1"

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
+/*********************************************************************************
+ *  WEB322 – Assignment 1
+ *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students.
+ * 
+ *  Name: _________Delisha Madhan _____________ Student ID: ______________ Date: __20th January, 2023______________
+ *
+ *  Cyclic Web App URL: ______________________________________________________
+ *
+ *  GitHub Repository URL: _____________________________________________________
+ *
+ ********************************************************************************/
+
+
+
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Delisha Madhan - 166471219");
 });
 
 // setup http server to listen on HTTP_PORTs
